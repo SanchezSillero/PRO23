7bf1d07262ead59ef7ac9acb76c3c3324ade9e56
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-03-20 17:01:23 +0100

    update

--- T4/T4_EjerciciosBase/T4_EjerciciosBase/src/Ejercicio5.java
+++ T4/T4_EjerciciosBase/T4_EjerciciosBase/src/Ejercicio5.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 /*Realiza un programa que pida 8 números enteros, los guarde en un array y que luego
   muestre esos números junto con la palabra “par” o “impar” según proceda.*/
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numeros[] = new int[8];
+
+        for (int i = 0; i < numeros; i++) {
+            
+
+        }
+    }
 }
