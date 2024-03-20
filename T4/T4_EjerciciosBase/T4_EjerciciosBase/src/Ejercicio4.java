0646fe35a318ea0af56441b2df0e5da1976cd8bb
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-03-20 16:42:13 +0100

    update

--- T4/T4_EjerciciosBase/T4_EjerciciosBase/src/Ejercicio4.java
+++ T4/T4_EjerciciosBase/T4_EjerciciosBase/src/Ejercicio4.java
@@ -4,25 +4,26 @@
     - En el array cubo se deben almacenar los cubos de los valores que hay en numero.*/
 public class Ejercicio4 {
     public static void main(String[] args) {
-        int[] numero = new int [20];  // definimos arrays
-        int[] cuadrado = new int [20];
-        int[] cubo = new int [20];
+        int[] numero = new int[20];  // definimos arrays
+        int[] cuadrado = new int[20];
+        int[] cubo = new int[20];
 
         for (int i = 0; i < numero.length; i++) { // damos valores aleatorios al array numeros
-            numero[i]=(int)(Math.random()*101);
-            System.out.print(numero[i] + " ");
+            numero[i] = (int) (Math.random() * 101);
         }
 
         System.out.println();
         for (int i = 0; i < cuadrado.length; i++) { //almacenamos los valores al cuadrado
-            cuadrado[i] = (numero[i]*numero[i]);
-            System.out.print(cuadrado[i]+" ");
+            cuadrado[i] = (int) (Math.pow(numero[i], 2));
         }
 
         System.out.println();
         for (int i = 0; i < cubo.length; i++) { //almacenamos los valores al cubo
-            cubo[i]= (numero[i]*numero[i]*numero[i]);
-            System.out.print(cubo[i]+ " ");
+            cubo[i] = (int) (Math.pow(numero[i], 3));
+        }
+
+        for (int i = 0; i < numero.length; i++) {//imprimos los valores en columnas
+            System.out.printf("Posición %d:   %-4d\t %-6d\t %-7d\n", i, numero[i], cuadrado[i], cubo[i]);
 
         }
     }
---
+++ T4/T4_EjerciciosBase/T4_EjerciciosBase/src/Ejercicio5.java
@@ -0,0 +1,4 @@
+/*Realiza un programa que pida 8 números enteros, los guarde en un array y que luego
+  muestre esos números junto con la palabra “par” o “impar” según proceda.*/
+public class Ejercicio5 {
+}
