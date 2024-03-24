f87373b78bd9da9899e94662e31adce1d29268f3
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-03-24 23:05:19 +0100

    update

--- T4/T4_EjerciciosClase/src/Ejercicio4.java
+++ T4/T4_EjerciciosClase/src/Ejercicio4.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
+/*4. (PosicionesArray) Crea una aplicación que:
+    - pida por teclado la longitud de un array
+    - cree el array de enteros de la longitud introducida
+    - pida al usuario que introduzca todos los datos del array
+    - mueva una posición todos los elementos. el elemento de la posición 0 pasa a la posición 1 y así sucesivamente. El elemento que está en la última posición pasa a la primera
+    - realizar la tarea anterior pero al contrario. El elemento que está en la primera posición pasa a la última y así sucesivamente*/
 public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int tamaño = teclado.nextInt();
+        int[] numeros = new int[tamaño];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = teclado.nextInt();
+        }
+        for (int item : numeros) {
+            System.out.print(item + " ");
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i]
+        }
+    }
 }
