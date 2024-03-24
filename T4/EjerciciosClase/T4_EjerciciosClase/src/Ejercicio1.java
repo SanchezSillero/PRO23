73dad7c94cc161e76cfebd82a2421697044d0946
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-03-24 19:15:17 +0100

    update

---
+++ T4/EjerciciosClase/T4_EjerciciosClase/src/Ejercicio1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/* (MultiplicarArray) Crear un array de 5 posiciones:
+    - se le pedirá al usuario que introduzca cada unos de los elementos del array
+    - Multiplica cada elemento del array * 2 y vuelve a guardar su valor
+    - Saca por consola la suma de todos los números
+    - Saca la media de todos los elementos*/
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int[] numeros = new int[5];
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Introduce un número entero");
+            numeros = 
+        }
+    }
+}
