710075deb176d7b7a6c436ee4abb352de207cb19
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-03-25 23:31:25 +0100

    update

--- T4/T4_EjerciciosClase/src/Ejercicio5.java
+++ T4/T4_EjerciciosClase/src/Ejercicio5.java
@@ -12,6 +12,7 @@ segundo al tercero, ..., y el último al primero).
   hay que hacer todo de golpe, pero opcionalmente se puede hacer un menú que indique cada una de las opciones*/
 public class Ejercicio5 {
     static int[] numeros = new int[10];
+
     public static void main(String[] args) {
         System.out.println("Generando 10 numeros aleatorios...");
         for (int i = 0; i < numeros.length; i++) {
@@ -40,21 +41,61 @@ public class Ejercicio5 {
 
     static public void duplicarPositivos() {
         for (int i = 0; i < numeros.length; i++) {
-            if (numeros[i]%2==0 && numeros[i]<5){
-                numeros[i]=numeros[i]*2;
+            if (numeros[i] % 2 == 0 && numeros[i] < 5) {
+                numeros[i] = numeros[i] * 2;
             }
         }
-
-
         mostrarArray();
     }
 
-    static public void sumarYrestar(){
+    static public void sumarYrestar() {
         for (int i = 0; i < numeros.length; i++) {
-            int aleatorio = (int)(Math.random()*11)-5;
-            numeros[i]+=aleatorio;
+            int aleatorio = (int) (Math.random() * 11) - 5;
+            numeros[i] += aleatorio;
         }
-    mostrarArray();
+        mostrarArray();
+    }
+
+    static public void moverDerecha() {
+        int aux = numeros[numeros.length - 1];
+        for (int i = numeros.length - 1; i > 0; i--) {
+            numeros[i] = numeros[i - 1];
+        }
+        numeros[0] = aux;
+        mostrarArray();
+    }
+
+    static public void intercambios() {
+        for (int i = 0; i < numeros.length - 1; i += 2) {
+            int aux = numeros[i];
+            numeros[i] = numeros[i + 1];
+            numeros[i + 1] = aux;
+        }
+        mostrarArray();
+    }
+
+    static public void invertir() {
+        System.out.println();
+        for (int i = numeros.length-1; i >= 0; i--) {
+            System.out.print(numeros[i]+", ");
+        }
+    }
+
+    static public void primerParUltimoImpar(){
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i]%2==0){
+                System.out.print("el primer par es "+numeros[i]);
+                break;
+            }
+        }
+        System.out.println();
+        for (int i = numeros.length-1; i >=0 ; i--) {
+            if (numeros[i]%2!=0){
+                System.out.print("el último impar es "+numeros[i]);
+                break;
+            }
+        }
+
     }
 
 }
