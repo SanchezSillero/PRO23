30105acbd77a52ba11ad99749ae753702e9dfb14
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-03-26 08:29:31 +0100

    update

--- T4/T4_EjerciciosClase/src/Ejercicio5.java
+++ T4/T4_EjerciciosClase/src/Ejercicio5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*(OperacionesArray) Crear un array con 10 posiciones y asígnale números enteros entre 0 y 9, ambos inclusive (de forma automática).
   Se mostrarán los datos separados por una coma y un espacio.
   Una vez hecho esto se modificará el array del de la manera indicada en cada apartado, y se mostrará de nuevo.
@@ -12,14 +14,62 @@ segundo al tercero, ..., y el último al primero).
   hay que hacer todo de golpe, pero opcionalmente se puede hacer un menú que indique cada una de las opciones*/
 public class Ejercicio5 {
     static int[] numeros = new int[10];
+    static int opcionMenu;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Generando 10 numeros aleatorios...\n");
+
+        System.out.println("Generando 10 numeros aleatorios...");
+
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = (int) (Math.random() * 10);
         }
-        System.out.println("\n¿Qué desea hacer? Seleccione una de las siguientes opciones:");
+
+        mostrarNumeros();
+
+        do {
+            mostrarMenu();
+            switch (opcionMenu) {
+                case 1:
+                    incrementarPares();
+                    break;
+
+                case 2:
+                    duplicarPositivos();
+                    break;
+
+                case 3:
+                    sumarYrestar();
+                    break;
+
+                case 4:
+                    moverDerecha();
+                    break;
+
+                case 5:
+                    intercambios();
+                    break;
+
+                case 6:
+                    invertir();
+                    break;
+
+                case 7:
+                    primerParUltimoImpar();
+                    break;
+
+                case 8:
+                    System.out.println("¡Hasta pronto!");
+                    break;
+                default:
+                    System.out.println("Opción inválida, por favor ingrese un número entre 1 y 8");
+            }
+
+        } while (opcionMenu != 8);
+
+
     }
+
 
     //Metodos para el menu
     public static void mostrarNumeros() {
@@ -28,6 +78,18 @@ public class Ejercicio5 {
         }
     }
 
+    public static void mostrarMenu() {
+        System.out.println("\n¿Qué desea hacer? Seleccione una de las siguientes opciones:\n" +
+                "1. Incrementar en 1 los valores pares y en -1 los impares.\n" +
+                "2. Duplicar los valores positivos menores que 5.\n" +
+                "3. Sumar a cada valor un valor entero aleatorio entre -5 y 5.\n" +
+                "4. Mover los datos una posición hacia la derecha.\n" +
+                "5. Intercambiar el primero con el segundo, el tercero con el cuarto, ..., el penúltimo con el último.\n" +
+                "6. Invertir el array.\n" +
+                "7. Mostrar la posición del primer par y del último impar.\n" +
+                "8. Salir.");
+        opcionMenu = scanner.nextInt();
+    }
 
     public static void incrementarPares() {
         for (int i = 0; i < numeros.length; i++) {
@@ -35,7 +97,7 @@ public class Ejercicio5 {
                 numeros[i] = numeros[i] + 1;
             } else numeros[i] = numeros[i] - 1;
         }
-
+        mostrarNumeros();
     }
 
     public static void duplicarPositivos() {
