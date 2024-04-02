c59a9d1b04f67231ddb0373cc7fc9028fd6d6b2c
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-04-03 00:33:39 +0200

    update

--- T6/EjerciciosBase/src/model/Persona.java
+++ T6/EjerciciosBase/src/model/Persona.java
@@ -8,6 +8,15 @@ public class Persona {
     private double peso;
 
     // métodos:
+    public void mostrarDatos() {
+        System.out.println("Nombre " + nombre);
+        System.out.println("Apellido " + apellido);
+        System.out.println("DNI " + dni);
+        System.out.println("Edad " + edad);
+        System.out.println("Peso " + peso);
+        System.out.println("Altura " + altura);
+    }
+
     //constructores:
 
     public Persona(String nombre, String apellido, String dni, int edad, int altura, double peso) {
@@ -27,27 +36,65 @@ public class Persona {
         this.edad = edad;
     }
 
-    public Persona(String nombre, String apellido){
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.dni="11111111X";
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = "11111111X";
     }
 
-    public Persona(){
+    public Persona() {
         this.nombre = "Por defecto";
         this.apellido = "Por defecto";
         this.dni = "11111111X";
     }
 
+    //getters y setters asociados
 
-    public void mostrarDatos() {
-        System.out.println("Nombre " + nombre);
-        System.out.println("Apellido " + apellido);
-        System.out.println("DNI " + dni);
-        System.out.println("Edad " + edad);
-        System.out.println("Peso " + peso);
-        System.out.println("Altura " + altura);
+    public String getNombre() {
+        return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 }
