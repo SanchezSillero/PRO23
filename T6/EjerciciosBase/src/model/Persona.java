6ee244a34a5e2ae2e46ccb656b7c95ec7d701119
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-04-02 23:23:52 +0200

    update

---
+++ T6/EjerciciosBase/src/model/Persona.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Persona {
+
+    // atributos:
+    private String nombre, apellido, dni;
+    private int edad, altura;
+    private double peso;
+
+  // métodos:
+    public void 
+
+
+}
