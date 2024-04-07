c5d8a779c9884d0bd3e71a4cfab334da787622ae
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-04-07 19:47:41 +0200

    update

---
+++ T8/T8/clase11/src/Entrada.java
@@ -0,0 +1,9 @@
+import model.Persona;
+
+public class Entrada {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        Persona persona1 = new Persona("133234", "Paco", "ñaldjgbngñd@gmail.com");
+
+    }
+}
