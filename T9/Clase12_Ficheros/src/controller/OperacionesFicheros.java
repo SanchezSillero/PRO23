deeb229107c2d357b1629946fd7817499e15f67f
FSanchezSillero <150432434+SanchezSillero@users.noreply.github.com>
2024-04-14 19:14:48 +0200

    update

--- T9/Clase12_Ficheros/src/controller/OperacionesFicheros.java
+++ T9/Clase12_Ficheros/src/controller/OperacionesFicheros.java
@@ -6,21 +6,43 @@ import java.io.IOException;
 public class OperacionesFicheros {
     public void lecturaInformacion(){
         //Obtener informaicon de un fichero
-        File file = new File("src/resources/informacion.txt");
-        // nombre, ruta, puedo leerlo, existe
-        System.out.println(file.getName());
+        File file = new File("src/resources");
+        // nombre, ruta, puedo leerlo, puedo ejecutarlo existe
+        /*System.out.println(file.getName());
         System.out.println(file.getAbsolutePath());
         System.out.println(file.canRead());
         System.out.println(file.canWrite());
         System.out.println(file.canExecute());
-        System.out.println(file.exists());
+        System.out.println(file.exists());*/
 
-        if (!file.exists() && file.isFile()){
+        /*if (!file.exists()){
             try {
-                file.createNewFile();
+                file.createNewFile();//crear fichero
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                System.out.println("Fallo en la creacion del fichero");
+            }
+
+        }*/
+
+        /*if (!file.exists()){
+            file.mkdir(); //crear un fichero; mkdirs, crea esa carpeta y las que no existan de la ruta proporcionada
+        }*/
+        if (file.isDirectory()){
+            //saca todos los nombres que tienes dentro
+           /* String[] nombres = file.list();
+            for (String nombre : nombres) {
+                System.out.println(nombre);
+            }*/
+                    
+             //saca todos los ficheros que tienes dentro //min 44 clase 13
+            File[] ficheros= file.listFiles();
+            for (File item : ficheros) {
+                if (item.isDirectory()){
+                    System.out.println("esto es un directectoi");
+                }
             }
         }
     }
+
+    public void lect
 }
